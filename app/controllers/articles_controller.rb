332659646dfd78cf66cf0d82a2df52103680b293
Aleksey Rykov <a.rykov8923@gmail.com>
2018-11-02 13:38:54 +1100

    Feature
    add forms for title, description and messengers checkboxes for new and edit articles views
    add output form for show and index articles view
    add article_params to articles_controller
    add one-to-many association between 'user' and 'article' models
    add user_id reference with new article at articles_controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,6 +25,7 @@ class ArticlesController < ApplicationController
   # POST /articles.json
   def create
     @article = Article.new(article_params)
+    @article.user = current_user
 
     respond_to do |format|
       if @article.save
@@ -69,6 +70,6 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.fetch(:article, {})
+      params.require(:article).permit(:title, :description, :user_id, messengers:[])
     end
 end
